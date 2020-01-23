feaee80a2d4048094d6a673b25a60230d950758d
Pheryus <plima@inf.ufsm.br>
2020-01-23 14:44:19 -0300

    Add new debug options

--- Assets/Scripts/DebugControl.cs
+++ Assets/Scripts/DebugControl.cs
@@ -8,33 +8,45 @@ public class DebugControl : MonoBehaviour {
 
     public Player player;
 
-    public TextMeshProUGUI dash, jump, dashSpeed, dashFrameDuration;
+    public TextMeshProUGUI dash, jump, startDashSpeed, framesConstantDash, framesAccelDash, framesDeccelDash, endDashSpeed;
 
     private void Update() {
 
-  
-        if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadPlus)) {
-            player.framesToConstantDashDuration++;
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			player.framesToAccelDash++;
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			player.framesToAccelDash--;
+		}
 
+		if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			player.framesToDeccelDash++;
+		} else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+			player.framesToDeccelDash--;
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.Alpha5)) {
+            player.framesToConstantDashDuration++;
         }
-        if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus)) {
+        if (Input.GetKeyDown(KeyCode.Alpha6)) {
             player.framesToConstantDashDuration--;
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9)) {
-            float prevTime = (player.framesToConstantDashDuration * player.maxDashSpeed) / 60;
-           
-            player.maxDashSpeed++;
-            player.startDashSpeed = player.maxDashSpeed / 2;
-            player.framesToConstantDashDuration = (int)((60 * prevTime) / player.maxDashSpeed);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0)) {
-            float prevTime = (player.framesToConstantDashDuration * player.maxDashSpeed) / 60;
 
-            player.maxDashSpeed--;
-            player.startDashSpeed = player.maxDashSpeed / 2;
-            player.framesToConstantDashDuration = (int)((60 * prevTime) / player.maxDashSpeed);
-        }
+		if (Input.GetKeyDown(KeyCode.Alpha7)) {
+			player.startDashSpeed++;
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha8)) {
+			player.startDashSpeed--;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Alpha9)) {
+			player.maxDashSpeed++;
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha0)) {
+			player.maxDashSpeed--;
+		}
 
         if (Input.GetKey(KeyCode.O)) {
             player.maxJumpHeight += Time.deltaTime * 1;
@@ -52,11 +64,33 @@ public class DebugControl : MonoBehaviour {
         else if (Input.GetKey(KeyCode.Escape))
             Application.Quit();
 
-        dash.text = "Dash Player Units: " + ((player.framesToConstantDashDuration * player.maxDashSpeed) / 60).ToString();
+        dash.text = "Dash Player Units: " + GetDashDistance().ToString();
         jump.text = "Jump Player Units: " + (player.maxJumpHeight / 1.5f).ToString();
-        dashSpeed.text = "Dash Speed ~: " + (player.maxDashSpeed.ToString());
+        startDashSpeed.text = "Start Dash Speed: " + (player.startDashSpeed.ToString());
+		endDashSpeed.text = "End Dash Speed: " + (player.maxDashSpeed.ToString());
+		
+		framesConstantDash.text = "Dash Frame Duration: " + player.framesToConstantDashDuration.ToString();
+		framesAccelDash.text = "Frames To Accel Dash:" + player.framesToAccelDash.ToString();
+		framesDeccelDash.text = "Frames To Deccel Dash:" + player.framesToDeccelDash.ToString();
+	}
 
-        dashFrameDuration.text = "Dash Frame Duration: " + player.framesToConstantDashDuration.ToString();
-    }
+	float GetDashDistance() {
+		float start = player.startDashSpeed;
+		float end = player.maxDashSpeed;
+		float t = player.framesToAccelDash;
+		float accel = (end - start) / t;
+
+		float dist1 = (end * end - start * start) / (2 * accel);
+
+		float dist2 = end * player.framesToConstantDashDuration;
+
+		start = end;
+		end = player.moveSpeed;
+		accel = (end - start) / player.framesToDeccelDash;
+		float dist3 = (end * end - start * start) / (2 * accel);
+
+		return (dist1 + dist2 + dist3) * Time.deltaTime;
+
+	}
 
 }
